2d926ada4886be710a047b60f6e85414102ea5a7
Zach <zachlightly@gmail.com>
2025-07-18 21:17:10 -0500

    Add some core info

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,17 @@
-import sys
+from fastapi import FastAPI
+from .db.db_setup import engine, Base
+# from .entities.todo import Todo         # Import models to register them
+# from .entities.user import User         # Import models to register them
+from .core.api import register_routes   
+from .core.logging import configure_logging, LogLevels     
 
+configure_logging(LogLevels.info)
 
-def print_python_executable():
-    print(f"Python executable location: {sys.executable}")
+app = FastAPI()     
 
+"""Only uncomment below to create new tables, 
+otherwise the tests will fail if not connected
+"""
+# Base.metadata.create_all(bind=engine)
 
-def main():
-    print("Hello from backend!")
-    print_python_executable()
-
-
-if __name__ == "__main__":
-    main()
+register_routes(app)
